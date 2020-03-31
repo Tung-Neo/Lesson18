f751181fc259b47d84a83a1fac3c556766eb2618
Tung-Neo <62163195+Tung-Neo@users.noreply.github.com>
2020-03-31 11:47:23 +0700

    Add files via upload

---
+++ A.java
@@ -0,0 +1,11 @@
+package Lesson18;
+
+public class A {
+    public A(){
+        System.out.println("Constructed an instance of A");
+    }
+    @Override
+    public String toString(){
+        return "This is A";
+    }
+}
---
+++ Monster.java
@@ -0,0 +1,12 @@
+package Lesson18;
+
+public class Monster {
+    private String name;
+
+    public Monster(String name){
+        this.name = name;
+    }
+    public String attack(){
+        return "!%$^$($^* I don't know how to attack!";
+    }
+}
